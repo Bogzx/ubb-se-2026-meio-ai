f10bd72566d6f42816d1a9d4abbe302882322b8c
tapordeimaia <maia.tapordei@gmail.com>
2026-04-06 16:19:19 +0300

    Refactor: Fix loop abbreviation in MusicSelectionDialogViewModel

--- Features/ReelsEditing/ViewModels/MusicSelectionDialogViewModel.cs
+++ Features/ReelsEditing/ViewModels/MusicSelectionDialogViewModel.cs
@@ -1,6 +1,6 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
-using System.Collections.ObjectModel;
 using Ubb_se_2026_meio_ai.Core.Models;
 using Ubb_se_2026_meio_ai.Features.ReelsEditing.Services;
 
@@ -8,26 +8,29 @@ namespace Ubb_se_2026_meio_ai.Features.ReelsEditing.ViewModels
 {
     public partial class MusicSelectionDialogViewModel : ObservableObject
     {
-        private readonly IAudioLibraryService _audioLibrary;
+        private readonly IAudioLibraryService audioLibrary;
 
         [ObservableProperty]
-        private ObservableCollection<MusicTrackModel> _availableTracks = new();
+        private ObservableCollection<MusicTrackModel> availableTracks = new();
 
         [ObservableProperty]
-        private MusicTrackModel? _selectedTrack;
+        private MusicTrackModel? selectedTrack;
 
         public MusicSelectionDialogViewModel(IAudioLibraryService audioLibrary)
         {
-            _audioLibrary = audioLibrary;
+            this.audioLibrary = audioLibrary;
         }
 
         [RelayCommand]
         private async Task LoadTracksAsync()
         {
-            var tracks = await _audioLibrary.GetAllTracksAsync();
+            var tracks = await audioLibrary.GetAllTracksAsync();
             AvailableTracks.Clear();
-            foreach (var t in tracks)
-                AvailableTracks.Add(t);
+
+            foreach (var musicTrack in tracks)
+            {
+                AvailableTracks.Add(musicTrack);
+            }
         }
 
         [RelayCommand]
